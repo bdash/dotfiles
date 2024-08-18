8cca303dacabeb79bf647480545ec831110e8981
Mark Rowe <mrowe@bdash.net.nz>
2024-08-18 15:20:30 -0700

    Override default indentation for YAML

---
+++ .vim/after/ftplugin/yaml.lua
@@ -0,0 +1,5 @@
+local indent = 2
+vim.opt_local.expandtab = true
+vim.opt_local.tabstop = indent
+vim.opt_local.softtabstop = indent
+vim.opt_local.shiftwidth = indent
--- .vim/init.lua
+++ .vim/init.lua
@@ -7,3 +7,4 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = indent
 vim.opt.softtabstop = indent
 vim.opt.tabstop = indent
+
